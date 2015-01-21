2f0adc8905229fa621aed990491ddb5a3713088d
EMILY FAN <emilyjustinefan@gmail.com>
2015-01-20 23:25:29 -0500

    Added more puts messages and calculated the total number of students for all cohorts.

--- exercise7.rb
+++ exercise7.rb
@@ -10,20 +10,30 @@ def display_cohort(students)
 	end
 end
 
+puts "Here are all the cohorts:"
+
 display_cohort(students)
 
 students[:cohort4] = 43
 
+puts "Here are just the cohorts:"
 puts students.keys
 
 def increase_by5(students)
-	students.each do |cohort, count|
+	students.each_value do |count|
 		puts (count * 1.05).to_i 
 	end
 end
 
+puts "The number of students for each cohort if we increased admissions by 5%:"
 increase_by5(students)
 
 students.delete(:cohort2)
 
+puts "Here are the remaining cohorts:"
 display_cohort(students)
+
+total = 0
+students.each_value {|count| total += count}
+
+puts "We have a total of #{total} students in all cohorts." 
