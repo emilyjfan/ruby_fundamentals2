9f1f08f860ee1d5f12ae525b8c3defff3680d168
EMILY FAN <emilyjustinefan@gmail.com>
2015-01-20 23:01:36 -0500

    increase students by 5%. deleted cohort 2.

--- exercise7.rb
+++ exercise7.rb
@@ -5,13 +5,25 @@ students = {
 }
 
 def display_cohort(students)
-	students.each do |cohort, students|
-		puts "#{cohort}: #{students}"
+	students.each do |cohort, count|
+		puts "#{cohort}: #{count}"
 	end
 end
 
 display_cohort(students)
 
 students[:cohort4] = 43
+
+puts students.keys
+
+def increase_by5(students)
+	students.each do |cohort, count|
+		puts (count * 1.05).to_i 
+	end
+end
+
+increase_by5(students)
+
+students.delete(:cohort2)
 
 display_cohort(students)
