0c6c588c6953803af99c9a1cc4c14ba6c2eccc66
EMILY FAN <emilyjustinefan@gmail.com>
2015-01-20 22:49:56 -0500

    added cohort 4.

--- exercise7.rb
+++ exercise7.rb
@@ -3,3 +3,15 @@ students = {
 	:cohort2 => 42,
 	:cohort3 => 22
 }
+
+def display_cohort(students)
+	students.each do |cohort, students|
+		puts "#{cohort}: #{students}"
+	end
+end
+
+display_cohort(students)
+
+students[:cohort4] = 43
+
+display_cohort(students)
